61faacf473749c12155a5ca0392d0c114bd2c8e3
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2023-05-07 18:55:58 -0300

    First Commit

---
+++ Sources/MeuLogSPM/Logger.swift
@@ -0,0 +1,14 @@
+//
+//  Logger.swift
+//  MeuLog
+//
+//  Created by Rodrigo Cerqueira Reis on 07/05/23.
+//
+
+import Foundation
+
+public class Logger {
+    static public func log(message: String) {
+        print("[LOGGER SPM] !!!: \(message)")
+    }
+}
